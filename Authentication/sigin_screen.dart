dedd640a850ca45e76a7e69aa6bcb66a68cff9c7
Swanand Joshi <swanandj2004@gmail.com>
2026-01-07 18:26:39 +0530

    Authentication added

---
+++ Authentication/sigin_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
+import 'register_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                await _authService.login(
+                  emailController.text.trim(),
+                  passwordController.text.trim(),
+                );
+              },
+              child: const Text("Login"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const RegisterScreen()),
+                );
+              },
+              child: const Text("Create Account"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
